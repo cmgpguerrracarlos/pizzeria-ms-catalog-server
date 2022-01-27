c315e88128c1a6815c27052f0193f68c74996fc2
Carlos Guerra <c.guerra@globant.com>
2022-01-27 19:20:48 -0300

    Update swagger

--- src/main/java/com/pizzeria/pizzeria/PizzeriaServiceApplication.java
+++ src/main/java/com/pizzeria/pizzeria/PizzeriaServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,7 +22,11 @@ public class PizzeriaServiceApplication {
 
 	@Bean
 	public Docket docket(){
-		return new Docket(DocumentationType.SWAGGER_2).select().build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.pizzeria.pizzeria"))
+				.build();
+
 	}
 
 }
